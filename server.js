16377fa86f0f3dff98bd33e00c17bc1df19ab3a5
avez <kubaolejnik3@gmail.com>
2025-04-22 17:01:45 +0200

    ds

--- server.js
+++ server.js
@@ -119,8 +119,8 @@ app.get("/badge.svg", async (req, res) => {
               <rect x="170" y="110" width="40" height="20" rx="6" ry="6" fill="#000000" filter="url(#shadow)"/>
               <text x="190" y="124" class="lang-tag" fill="white" text-anchor="middle">👀 ${project.watchers}</text>
 
-              <rect x="70" y="140" width="130" height="20" rx="6" ry="6" fill="${bgColor}" filter="url(#shadow)"/>
-              <text x="140" y="154" class="lang-tag" fill="${textColor}" text-anchor="middle">🔗 <a href="${project.url}" style="fill: ${textColor}; text-decoration: none;">${project.url}</a></text>
+              <rect x="70" y="140" width="140" height="20" rx="6" ry="6" fill="#58a6ff" filter="url(#shadow)"/>
+              <text x="140" y="154" class="lang-tag" fill="white" text-anchor="middle">🔗 <a href="${project.url}" style="fill: ${textColor}; text-decoration: none;">Zobacz Projekt</a></text>
 
             </g>
           `
